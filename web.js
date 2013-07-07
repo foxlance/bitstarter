e231f35db6d64c4bdf955c5cc9df1636ed627391
Lance Vincent Bumanglag <lancevincent@gmail.com>
2013-07-07 14:23:54 +0800

    revised web.js to use readFileSync and Buffer functions from node.js

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	//response.send('Hello World 2!');
+	//var content = fs.readFileSync('index.html');
+	//response.send(content);
+	var content = fs.readFileSync('index.html', 'utf8');
+
+	var buffer = new Buffer(256);
+	len = buffer.write(content, 0);
+	response.send(buffer.toString('utf8', 0, len));
+	
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
